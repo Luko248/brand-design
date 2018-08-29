2d38fc4c1a093942eec6b4c7795e2c2271d2e77c
Lukáš Chylík <lukas.chylik@riganti.cz>
2018-08-29 21:25:03 +0200

    Resources added, index created

---
+++ Brand-Design/Resources/JS/Init.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var HeaderHeight = $('header.grid-header__landing').innerHeight();
+    var Nav = $('nav');
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= HeaderHeight) {
+            Nav.addClass("fixed");
+        }
+        else {
+            Nav.removeClass("fixed");
+        }
+    });
+
+    $('.burger-button').click(function () {
+        $(this).toggleClass('open');
+        $('nav').toggleClass('nav-open');
+    });
+
+    //$('.slider').slick({
+    //    infinite: true,
+    //    slidesToShow: 1,
+    //    slidesToScroll: 1,
+    //    speed: 1000,
+    //    autoplay: true,
+    //    autoplaySpeed: 4000,
+    //    dots: false,
+    //    fade: true
+    //});
+});
