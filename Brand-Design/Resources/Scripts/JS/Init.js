146f3c484b470b8119fe8055ca66c7b0c6bd7855
Lukáš Chylík <lukas.chylik@riganti.cz>
2018-09-04 20:28:13 +0200

    navigation has been rewrited to BEm , JS included

--- Brand-Design/Resources/Scripts/JS/Init.js
+++ Brand-Design/Resources/Scripts/JS/Init.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
     var HeaderHeight = $('.header--landing').height();
-    var Nav = $('.navigation');
+    var Nav = $('.nav');
+    var Main = $('main');
+    var Menu = $('.nav__menu');
     $('.burger-button').click(function () {
-        $(this).toggleClass('open');
-        $('nav').toggleClass('nav-open');
+        $(this).toggleClass('burger-button--open');
+        Menu.toggleClass('nav__menu--open');
     });
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > HeaderHeight) {
-            Nav.addClass("fixed");
+            Nav.addClass("nav--fixed");
+            Main.css("margin-top", "5rem");
         }
         else {
-            Nav.removeClass("fixed");
+            Nav.removeClass("nav--fixed");
+            Main.css("margin-top", "0");
         }
     });
 });
