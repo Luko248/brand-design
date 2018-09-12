568131ab5117301f37d78151f23a01a5e1421aa2
Lukáš Chylík <lukas.chylik@riganti.cz>
2018-09-12 21:23:56 +0200

    scrolling added

--- Brand-Design/Resources/Scripts/Init.ts
+++ Brand-Design/Resources/Scripts/Init.ts
@@ -1,6 +1,7 @@
 ﻿var HeaderHeight = $('.header--landing').height();
 var Nav = $('.nav');
 var Main = $('main');
+var Header = $('header');
 var Menu = $('.nav__menu');
 
 $(document).ready(function () {
@@ -10,7 +11,7 @@ $(document).ready(function () {
         Menu.toggleClass('nav__menu--open');
     });
 
-    if ($(window).width() > 768) {
+    if ($(window).width() > 768 && Header.hasClass('header--landing')) {
         $(window).scroll(function () {
             var scroll = $(window).scrollTop();
             if (scroll > 50) {
@@ -24,5 +25,28 @@ $(document).ready(function () {
     else {
         Nav.addClass("nav--fixed");
     }
-   
+
+    $('#HomeScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.header--landing').offset().top
+        }, 1000);
+    });
+
+    $('#AboutScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--about').offset().top
+        }, 1000);
+    });
+
+    $('#ReferencesScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--references').offset().top
+        }, 1000);
+    });
+
+    $('#ContactScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--contact').offset().top
+        }, 1000);
+    });
 });
--- Brand-Design/Resources/Scripts/JS/Init.js
+++ Brand-Design/Resources/Scripts/JS/Init.js
@@ -221,13 +221,14 @@ function conferenceMap() {
 var HeaderHeight = $('.header--landing').height();
 var Nav = $('.nav');
 var Main = $('main');
+var Header = $('header');
 var Menu = $('.nav__menu');
 $(document).ready(function () {
     $('.burger-button').click(function () {
         $(this).toggleClass('burger-button--open');
         Menu.toggleClass('nav__menu--open');
     });
-    if ($(window).width() > 768) {
+    if ($(window).width() > 768 && Header.hasClass('header--landing')) {
         $(window).scroll(function () {
             var scroll = $(window).scrollTop();
             if (scroll > 50) {
@@ -241,5 +242,25 @@ $(document).ready(function () {
     else {
         Nav.addClass("nav--fixed");
     }
+    $('#HomeScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.header--landing').offset().top
+        }, 1000);
+    });
+    $('#AboutScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--about').offset().top
+        }, 1000);
+    });
+    $('#ReferencesScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--references').offset().top
+        }, 1000);
+    });
+    $('#ContactScroll').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.section--contact').offset().top
+        }, 1000);
+    });
 });
 //# sourceMappingURL=Init.js.map
